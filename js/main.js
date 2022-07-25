093251d13634cc17c32917d748685700a843ed3c
Shakha <shaxapilot@gmail.com>
2022-07-25 19:31:56 +0500

    finished tasks

--- js/main.js
+++ js/main.js
@@ -1,65 +1,113 @@
 /* Sizga users array beriladi, siz funksiya yasaysiz, 
-u funksiyaga users arrayni va telefon nomer kiritaman. 
-Va u menga returnida shu nomer kimga tegishliligini 
-chiqarib beradi. Agar yo'q bo'lsa bo'sh string qaytaradi.
+ u funksiyaga users arrayni va telefon nomer kiritaman. 
+ Va u menga returnida shu nomer kimga tegishliligini 
+ chiqarib beradi. Agar yo'q bo'lsa bo'sh string qaytaradi.
+*/
+
+
+// let users = [
+//     {
+//         name: "Avazbek",
+//         telNumber: "998911645099"
+//     },
+//     {
+//         name: "Umid",
+//         telNumber: "998911758585"
+//     },
+//     {
+//         name: "O'ktam",
+//         telNumber: "998911777767"
+//     },
+//     {
+//         name: "Valijon",
+//         telNumber: "998917777777"
+//     },
+//     {
+//         name: "Sarvar",
+//         telNumber: "998911222222"
+//     },
+//     {
+//         name: "Sardor",
+//         telNumber: "998911222322"
+//     },
+//     {
+//         name: "Toxir",
+//         telNumber: "998911645022"
+//     }
+// ]
+
+// // code shu yerda yozilgan `downarrow`
+
+// let userNum = prompt("Please enter a number")
+
+// function findUser(array, number) {
+//     let user = ""
+
+//     for (let i = 0; i < array.length; i++) {
+//         if(number == array[i].telNumber)
+//         return alert(`${users[i].name}`)
+//     }
+// }
+
+
+// findUser(users, userNum);
+
+
+
+// TASK 2
+
+/* Sizga users array beriladi, siz funksiya yasaysiz, 
+ u funksiyaga users arrayni va (22)yosh kiritaman. 
+ Va u menga returnida yangi array qaytaradi va 
+ unda faqat 22 yoshga teng va undan kattalarni qaytaradi.
 */
 let users = [
     {
         name: "Avazbek",
-        telNumber: "998911645099"
+        age: "25"
     },
     {
         name: "Umid",
-        telNumber: "998911758585"
+        age: "21"
     },
     {
         name: "O'ktam",
-        telNumber: "998911777767"
+        age: "22"
     },
     {
         name: "Valijon",
-        telNumber: "998917777777"
+        age: "19"
     },
     {
         name: "Sarvar",
-        telNumber: "998911222222"
+        age: "27"
     },
     {
         name: "Sardor",
-        telNumber: "998911222322"
+        age: "17"
     },
     {
         name: "Toxir",
-        telNumber: "998911645022"
+        age: "16"
     }
 ]
 
-// code shu yerda yozilgan `downarrow`
+let personAge = prompt("Type your age:")
 
-let userNum = prompt("Please enter a number")
+function ageGreaterThan(array, minAge) {
+    let newArray = []
 
-function findUser(array, number) {
-    let user = ""
-    
-    for (let i = 0; i < array.length; i++) {
-        if ( number == users[i].telNumber);
-        user = users[i].name
+    for (let i = 0; i < array.length; i++)
+        if (personAge <= users[i].age){
+            newArray.push(users[i])
+        }
+        
+        return console.log(newArray);
     }
-    return user
-}
-
-findUser(users, userNum)
-
-
-
-console.log(findUser(users, userNumber));
-alert(findUser(users, userNumber))
-
-
-// Result
 
 
 
 
+ageGreaterThan(users, 22)
 
 
